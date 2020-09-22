cffe175a7ab1c9384e600843e6f51ec81599a0e0
Uzias Rivera <uziasrivera@gmail.com>
2020-09-21 23:42:36 -0700

    styling login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,23 @@
-import React , { useState } from 'react';
+import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 
 const Login = () => {
 
     const [user, setUser] = useState({
-        email:"",
-        password:""
+        email: "",
+        password: ""
     })
 
     return (
-        <div>
-          <TextField variant="outlined" placeholder="email" name="email" value={user.email} onChange={(e)=>setUser({...user, email: e.target.value})}/>
-          <TextField variant="outlined" placeholder="password" name="password" value={user.password} onChange={(e)=>setUser({...user, password: e.target.value})}/>
+        <div style={{ width: "30%" }}>
+            <Typography variant="h2" style={{ color: "black" }}>Login</Typography>
+            <div style={{ display: "flex", flexDirection: "column", margin:"10% 0" }}>
+                <Typography variant="h5" style={{ color: "black", textAlign:"left" }}>Email</Typography>
+                <TextField variant="outlined" placeholder="enter email" name="email" value={user.email} onChange={(e) => setUser({ ...user, email: e.target.value })} />
+                <Typography variant="h5" style={{ color: "black", textAlign:"left", marginTop:"5%" }}>Password</Typography>
+                <TextField variant="outlined" placeholder="enter password" name="password" value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} />
+            </div>
         </div>
     );
 };
