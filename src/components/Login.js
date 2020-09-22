64640b00cd397654b3ee6f05b12ca35791311721
Uzias Rivera <uziasrivera@gmail.com>
2020-09-21 23:46:52 -0700

    finished styling login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 const Login = () => {
 
@@ -12,12 +13,13 @@ const Login = () => {
     return (
         <div style={{ width: "30%" }}>
             <Typography variant="h2" style={{ color: "black" }}>Login</Typography>
-            <div style={{ display: "flex", flexDirection: "column", margin:"10% 0" }}>
+            <div style={{ display: "flex", flexDirection: "column", margin:"5% 0 5% 0" }}>
                 <Typography variant="h5" style={{ color: "black", textAlign:"left" }}>Email</Typography>
                 <TextField variant="outlined" placeholder="enter email" name="email" value={user.email} onChange={(e) => setUser({ ...user, email: e.target.value })} />
                 <Typography variant="h5" style={{ color: "black", textAlign:"left", marginTop:"5%" }}>Password</Typography>
                 <TextField variant="outlined" placeholder="enter password" name="password" value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} />
             </div>
+            <Button variant="contained" color="primary" style={{ cursor: "pointer", width: "100%" }} disabled={user.email === "" || user.password === ""}>Login</Button>
         </div>
     );
 };
