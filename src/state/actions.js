2eb0085bcbf59117ad1a573bc415b42173f1997a
Uzias Rivera <uziasrivera@gmail.com>
2020-09-28 16:35:20 -0700

    adding actions and fleshing out reducer

--- src/state/actions.js
+++ src/state/actions.js
@@ -4,9 +4,21 @@ export const GET_JOBS_START = "GET_JOBS_START"
 export const GET_JOBS_SUCCESS = "GET_JOBS_SUCCESS"
 export const GET_JOBS_FAIL = "GET_JOBS_FAIL"
 
+export const POST_JOB_START = "POST_JOB_START"
+export const POST_JOB_SUCCESS = "POST_JOB_SUCCESS"
+export const POST_JOB_FAIL = "POST_JOB_FAIL"
+
 export const getJobs = () => dispatch => {
     dispatch({ type: GET_JOBS_START, })
     authorizedAxios().get("/jobs")
         .then(res => dispatch({ type: GET_JOBS_SUCCESS, payload: res.data }))
         .catch(err => dispatch({ type: GET_JOBS_SUCCESS, payload: err }))
+}
+
+export const addJob = (job) => dispatch => {
+    dispatch({ type: POST_JOB_START })
+    authorizedAxios().post("/", job)
+        .then(res => dispatch({ type: POST_JOB_SUCCESS, payload: res.data }))
+        .catch(err => dispatch({ type: POST_JOB_FAIL, payload: err }))
+
 }
--- src/state/jobs.js
+++ src/state/jobs.js
@@ -1,14 +1,65 @@
-const initialState = {
+import {
+    GET_JOBS_START,
+    GET_JOBS_SUCCESS,
+    GET_JOBS_FAIL,
+    POST_JOB_START,
+    POST_JOB_SUCCESS,
+    POST_JOB_FAIL,
+}
+    from "./actions"
 
+const initialState = {
+    job: {},
+    jobs: [],
+    error: null,
+    loading: false
 }
 
-const jobsReducer = (state=initialState, action) => {
+const jobsReducer = (state = initialState, action) => {
     switch (action) {
-        case "":{
-            return {}
+        case GET_JOBS_START: {
+            return {
+                ...state,
+                loading: true,
+                error: null
+            }
+        }
+        case GET_JOBS_SUCCESS: {
+            return {
+                ...state,
+                loading: false,
+                jobs: action.payload
+            }
+        }
+        case GET_JOBS_FAIL: {
+            return {
+                ...state,
+                loading: false,
+                error: action.payload
+            }
+        }
+        case POST_JOB_START: {
+            return {
+                ...state,
+                loading: false,
+                error: null
+            }
+        }
+        case POST_JOB_SUCCESS: {
+            return {
+                ...state,
+                loading: false,
+            }
+        }
+        case POST_JOB_FAIL: {
+            return {
+                ...state,
+                loading: false,
+                error: action.payload
+            }
         }
         default: {
             return initialState
-        } 
-    } 
+        }
+    }
 }
