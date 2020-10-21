c333b90e11b69167e23dcd039525938457fd9334
Uzias Rivera <uziasrivera@gmail.com>
2020-10-20 23:14:12 -0700

    styled JobBoard.js

--- src/components/JobBoard.js
+++ src/components/JobBoard.js
@@ -42,20 +42,24 @@ const JobBoard = () => {
 
     return (
         <div className="jobBoardRoot">
-            <div style={{ display: "flex", flexDirection: "column", alignItems: "center", alignContent: "center", width: "500px" }}>
-                <form onSubmit={(e) => searchHandler(e)} style={{ display: "flex", justifyContent: "space-between", alignContent: "center", alignItems: "center", width: "100%", margin: "1% auto" }}>
-                    <TextField value={query} onChange={(e) => setQuery(e.target.value)} label="Job Title, Company, or Location" style={{ width: "80%", fontSize: "32px", padding: "3%" }}></TextField>
-                    <div style={{ display: "flex", justifyContent: "space-between", width: "50%" }}>
-                        <Button type="submit" style={{ margin: "1% auto" }} variant="contained" color="primary">Search</Button>
-                        <Button className="filterIcon" onClick={() => setOpenFilters(!openFilters)}>
-                            <FontAwesomeIcon icon={faFilter} style={{ color: "#3f51b5", fontSize: "28px" }} />
-                        </Button>
+            {state.loading ? <Typography variant="h2" style={{ color: "black", marginTop: "5%" }}>Loading...</Typography> :
+                <>
+                    <Typography variant="h3" style={{ color: "black", margin: ".25% auto" }}>Follow up... get hired</Typography>
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", alignContent: "center", width: "500px" }}>
+                        <form onSubmit={(e) => searchHandler(e)} style={{ display: "flex", justifyContent: "space-between", alignContent: "center", alignItems: "center", width: "100%", margin: "1% auto" }}>
+                            <TextField value={query} onChange={(e) => setQuery(e.target.value)} label="Job Title, Company, or Location" style={{ width: "80%", fontSize: "32px", padding: "3%" }}></TextField>
+                            <div style={{ display: "flex", justifyContent: "space-between", width: "50%" }}>
+                                <Button type="submit" style={{ margin: "1% auto" }} variant="contained" color="primary">Search</Button>
+                                <Button className="filterIcon" onClick={() => setOpenFilters(!openFilters)}>
+                                    <FontAwesomeIcon icon={faFilter} style={{ color: "#3f51b5", fontSize: "28px" }} />
+                                </Button>
+                            </div>
+                        </form>
+                        {openFilters ? <JobBoardFilters setOpenFilters={setOpenFilters} /> : null}
+                        {state.jobs.length && state.initialJobsLoad ? <JobCard jobs={filteredData} /> : <Typography variant="h2" style={{ color: "black", marginTop: "5%" }}>There are no jobs</Typography>}
                     </div>
-                </form>
-                {openFilters ? <JobBoardFilters setOpenFilters={setOpenFilters} /> : null}
-                {state.loading ? <Typography variant="h2" style={{ color: "black", marginTop: "5%" }}>Loading...</Typography>
-                    : state.jobs.length && state.initialJobsLoad ? <JobCard jobs={filteredData} /> : <Typography variant="h2" style={{ color: "black", marginTop: "5%" }}>There are no jobs</Typography>}
-            </div>
+                </>
+            }
         </div>
     );
 };
