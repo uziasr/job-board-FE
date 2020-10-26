e209515439af8d4601558cb8783c5218e426b8d9
Uzias Rivera <uziasrivera@gmail.com>
2020-10-25 23:57:14 -0700

    adding edit feature

--- src/components/Job.js
+++ src/components/Job.js
@@ -2,10 +2,14 @@ import React, { useState, useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { getJobById, updateJob } from "../state/actions"
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
 import { Link } from "react-router-dom"
 
 const Job = (props) => {
@@ -21,17 +25,22 @@ const Job = (props) => {
         5: "dream job"
     })
 
+    const [status, setStatus] = useState(["applied", "interviewing", "reject", "declined", "hired"])
+
     const updateStatus = (status) => {
         dispatch(updateJob(url, { status: status }))
     }
-    
+
     const [jobDetails, setJobDetails] = useState({})
     const [edit, setEdit] = useState(false)
 
     useEffect(() => {
-        dispatch(getJobById(url))
-        setJobDetails(state.job)
-    }, [])
+        if (state.job.company === "") {
+            dispatch(getJobById(url))
+        } else {
+            setJobDetails({ ...state.job })
+        }
+    }, [state.job])
 
     console.log(jobDetails)
 
@@ -94,40 +103,100 @@ const Job = (props) => {
                         <div className="jobDetailsWrap">
                             <div className="jobDetails">
                                 <Typography className="jobDetailTitle">COMPANY</Typography>
-                                <Typography variant="h6" className="jobDetailText">{state.job.company}</Typography>
+                                {edit ? <TextField
+                                    name="company"
+                                    variant="outlined"
+                                    color="primary"
+                                    value={jobDetails.company}
+                                    className="jobInput"
+                                    onChange={e => setJobDetails({ ...jobDetails, [e.target.name]: e.target.value })}
+                                ></TextField> : <Typography variant="h6" className="jobDetailText">{state.job.company}</Typography>}
                             </div>
                             <div className="jobDetails">
                                 <Typography className="jobDetailTitle">LOCATION</Typography>
-                                <Typography variant="h6" className="jobDetailText">{state.job.location}</Typography>
+                                {edit ? <TextField
+                                    name="location"
+                                    variant="outlined"
+                                    color="primary"
+                                    value={jobDetails.location}
+                                    className="jobInput"
+                                    onChange={e => setJobDetails({ ...jobDetails, [e.target.name]: e.target.value })}
+                                ></TextField> : <Typography variant="h6" className="jobDetailText">{state.job.location}</Typography>}
                             </div>
                             <div className="jobDetails">
                                 <Typography className="jobDetailTitle">SALARY</Typography>
-                                <Typography variant="h6" className="jobDetailText">{state.job.salary}</Typography>
+                                {edit ? <TextField
+                                    name="salary"
+                                    variant="outlined"
+                                    color="primary"
+                                    value={jobDetails.salary}
+                                    className="jobInput"
+                                    onChange={e => setJobDetails({ ...jobDetails, [e.target.name]: e.target.value })}
+                                ></TextField> : <Typography variant="h6" className="jobDetailText">{state.job.salary}</Typography>}
                             </div>
                             <div className="jobDetails">
                                 <Typography className="jobDetailTitle">IMPORTANCE</Typography>
-                                <Typography variant="h6" className="jobDetailText">{importance[state.job.importance]}</Typography>
+                                {edit ?
+                                    <FormControl style={{ width: "100%" }}>
+                                        <Select
+                                            value={jobDetails.importance}
+                                            label="Importance"
+                                            className="jobInput"
+                                            name="importance"
+                                            onChange={(e) => setJobDetails({ ...jobDetails, importance: Number(e.target.value) })}
+                                            style={{ width: "100%" }}
+                                        >
+                                            {Object.keys(importance).map(i => (
+                                                <MenuItem key={i} value={i}>{importance[i]}</MenuItem>
+                                            ))}
+                                        </Select>
+                                    </FormControl>
+                                    : <Typography variant="h6" className="jobDetailText">{importance[state.job.importance]}</Typography>}
                             </div>
                             <div className="jobDetails">
                                 <Typography className="jobDetailTitle">STATUS</Typography>
-                                <Typography variant="h6" className="jobDetailText">{state.job.status}</Typography>
+                                {edit ?
+                                    <FormControl style={{ width: "100%" }}>
+                                        <Select
+                                            value={jobDetails.status}
+                                            label="Status"
+                                            className="jobInput"
+                                            name="status"
+                                            onChange={(e) => setJobDetails({ ...jobDetails, status: e.target.value })}
+                                            style={{ width: "100%" }}
+                                        >
+                                            {status.map(i => (
+                                                <MenuItem key={i} value={i}>{i}</MenuItem>
+                                            ))}
+                                        </Select>
+                                    </FormControl>
+                                    : <Typography variant="h6" className="jobDetailText">{state.job.status}</Typography>}
                             </div>
                             <div className="jobDetails">
-                                <Typography className="jobDetailTitle">DATE</Typography>
+                                <Typography className="jobDetailTitle">DATE APPLIED</Typography>
                                 <Typography variant="h6" className="jobDetailText">{state.job.date.split(" ").splice(1, 3).join(' ')}</Typography>
                             </div>
                             <div className="jobDetails">
                                 <Typography className="jobDetailTitle">FOLLOWED UP</Typography>
-                                <Typography variant="h6" className="jobDetailText">No</Typography>
+                                {edit ?
+                                    <div style={{ display: "flex" }}>
+                                        <div onClick={() => setJobDetails({ ...jobDetails, followed_up: true })} className="jobEditButton" style={{ background: jobDetails.followed_up === true ? "#3CB371" : "white", border: jobDetails.followed_up === true ? "none" : "1px solid #3CB371", color: jobDetails.followed_up === true ? "white" : "#3CB371" }}>
+                                            <Typography>Yes</Typography>
+                                        </div>
+                                        <div onClick={() => setJobDetails({ ...jobDetails, followed_up: false })} className="jobEditButton" style={{ background: jobDetails.followed_up === false ? "#3CB371" : "white", border: jobDetails.followed_up === false ? "none" : "1px solid #3CB371", color: jobDetails.followed_up === false ? "white" : "#3CB371" }}>
+                                            <Typography>No</Typography>
+                                        </div>
+                                    </div>
+                                    : <Typography variant="h6" className="jobDetailText">{state.job.followed_up ? "Yes" : "No"}</Typography>}
                             </div>
-                            {edit ? <div style={{ display: "flex", justifyContent: "center", margin: "0 auto", width: "90%" }}>
-                                <div className="jobEditButton" onClick={()=>setEdit(false)}>
+                            {edit ? <div style={{ display: "flex", width: "90%" }}>
+                                <div className="jobEditButton" onClick={() => setEdit(false)}>
                                     <Typography>CANCEL</Typography>
                                 </div>
                                 <div className="jobEditButton">
                                     <Typography>SUBMIT</Typography>
                                 </div>
-                            </div> : <div onClick={()=>setEdit(true)} className="jobDetailsButton">
+                            </div> : <div onClick={() => setEdit(true)} className="jobDetailsButton">
                                     <Typography>EDIT</Typography>
                                 </div>}
                         </div>
---
+++ src/components/JobEdit.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const JobEdit = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default JobEdit;
